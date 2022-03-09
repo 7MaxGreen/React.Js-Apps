03e5b40f73cd6a929f467c831d575828a1e903a1
Max <maximnicola7@gmail.com>
2022-03-09 20:03:57 +0200

    First commit

---
+++ hangman/src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// rafce
+const Header = () => {
+  return (
+    <>
+      <h1>Hangman</h1>
+      <p>Find the hidden word - Enter a letter</p>
+      <p>C O L O R S</p>
+    </>
+  )
+}
+
+export default Header
